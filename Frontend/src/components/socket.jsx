2fec89c0afb189c04e6203529ef8dc194a2010e7
Ashish Vaghela <ashish.vaghela@egreycell.com>
2024-05-16 13:04:53 +0530

    update backend connection url

--- Frontend/src/components/socket.jsx
+++ Frontend/src/components/socket.jsx
@@ -2,7 +2,7 @@
 
 import io from 'socket.io-client';
 
-const ENDPOINT = 'http://54.167.62.6:5000'; // added backend url
+const ENDPOINT = 'https://stalk-talk.onrender.com'; // added backend url
 // const ENDPOINT = 'http://localhost:5001'; // Updated to match the backend port
 
 const socket = io(ENDPOINT);
